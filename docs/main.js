eee2bd3ff67045728c8251c9135d119ab38e64a3
tamakiii <tamakiii@users.noreply.github.com>
2019-03-20 20:36:50 +0900

    display-with-button

--- docs/main.js
+++ docs/main.js
@@ -3,5 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('button[name="hello"]').onclick = () => {
     alert('hello');
   };
+  document.querySelector('button[name="display"]').onclick = () => {
+    document.querySelector('div[style="display: none; visibility: hidden;"]').style = "";
+  };
 });
 
